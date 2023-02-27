8f84cd953f01606c3d6f0efe658ddb9bc21f96a7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-02-27 15:22:21 +0300

    sanctum

---
+++ tests/Feature/LifeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LifeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->post(
+            '/api/state',
+
+            [ 'url' => 'https://test.ru/main' ]
+        );
+
+        $response->assertStatus(200);
+    }
+}
